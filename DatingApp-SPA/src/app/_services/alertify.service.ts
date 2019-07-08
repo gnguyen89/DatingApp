55e13c848cac7657bdf8f6546615ffe32ab4f9e7
Giang Nguyen <luckytintinnz@yahoo.com>
2019-07-08 14:48:36 +1200

    Module6 (#3)
    * Added toasts
    
    * End of module 6

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+declare let alertify: any; // Bypassing linting, at this point alertify has already been imported globally
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+  constructor() { }
+
+  confirm(message: string, okCallback: () => any) {
+    alertify.confirm(message, (e) => {
+      if (e) {
+        okCallback();
+      } else {}
+    });
+  }
+
+  success(message: string) {
+    alertify.success(message);
+  }
+  error(message: string) {
+    alertify.error(message);
+  }
+  warning(message: string) {
+    alertify.warning(message);
+  }
+  message(message: string) {
+    alertify.message(message);
+  }
+}
